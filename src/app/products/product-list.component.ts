0bb526104bfae5b9bfd3f72b9cc766c59b0789a2
avuthuajay <avuthuajay@gmail.com>
2019-11-10 10:14:05 +0530

    Populating data using ngif

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,4 +6,26 @@ import { Component} from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle = 'Product List';
+    products: any[] = [
+        {
+            productId: 1,
+            productName: 'Bag',
+            productCode: 'BG-001',
+            releaseDate: 'March 18, 2019',
+            description: '15 kg holding capacity',
+            price: 1299.99,
+            starRating: 4.2,
+            imageUrl: ''
+        },
+        {
+            productId: 2,
+            productName: 'Book',
+            productCode: 'BK-001',
+            releaseDate: 'March 20, 2019',
+            description: 'Childer bed time stories',
+            price: 129.99,
+            starRating: 4.5,
+            imageUrl: ''
+        }
+    ];
 }
