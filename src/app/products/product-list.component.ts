3acafc697aae5abb8dd64d3680c319c524b20e69
avuthuajay <avuthuajay@gmail.com>
2019-11-10 10:09:09 +0530

    Product List

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'ajay-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+    pageTitle = 'Product List';
+}
